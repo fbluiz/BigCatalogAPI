b5615d40c78df4ce2d8f93d43f3ba25772dc301c
Fabio Luiz <fbluiz.sf@gmail.com>
2022-11-21 11:44:50 -0300

    Modelo de dominio e contexto

---
+++ BigCatalogAPI/Context/NorthwindContext.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using BigCatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigCatalogAPI.Context;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    {
+    }
+
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Supplier> Suppliers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-332I3M0\\SQLEXPRESS;Database=Northwind;Encrypt=False; Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasIndex(e => e.CategoryName, "CategoryName");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName).HasMaxLength(15);
+            entity.Property(e => e.Description).HasColumnType("ntext");
+            entity.Property(e => e.Picture).HasColumnType("image");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasIndex(e => e.CategoryId, "CategoriesProducts");
+
+            entity.HasIndex(e => e.CategoryId, "CategoryID");
+
+            entity.HasIndex(e => e.ProductName, "ProductName");
+
+            entity.HasIndex(e => e.SupplierId, "SupplierID");
+
+            entity.HasIndex(e => e.SupplierId, "SuppliersProducts");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.ProductName).HasMaxLength(40);
+            entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
+            entity.Property(e => e.ReorderLevel).HasDefaultValueSql("((0))");
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+            entity.Property(e => e.UnitPrice)
+                .HasDefaultValueSql("((0))")
+                .HasColumnType("money");
+            entity.Property(e => e.UnitsInStock).HasDefaultValueSql("((0))");
+            entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("((0))");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_Products_Categories");
+
+            entity.HasOne(d => d.Supplier).WithMany(p => p.Products)
+                .HasForeignKey(d => d.SupplierId)
+                .HasConstraintName("FK_Products_Suppliers");
+        });
+
+        modelBuilder.Entity<Supplier>(entity =>
+        {
+            entity.HasIndex(e => e.CompanyName, "CompanyName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+            entity.Property(e => e.Address).HasMaxLength(60);
+            entity.Property(e => e.City).HasMaxLength(15);
+            entity.Property(e => e.CompanyName).HasMaxLength(40);
+            entity.Property(e => e.ContactName).HasMaxLength(30);
+            entity.Property(e => e.ContactTitle).HasMaxLength(30);
+            entity.Property(e => e.Country).HasMaxLength(15);
+            entity.Property(e => e.Fax).HasMaxLength(24);
+            entity.Property(e => e.HomePage).HasColumnType("ntext");
+            entity.Property(e => e.Phone).HasMaxLength(24);
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+            entity.Property(e => e.Region).HasMaxLength(15);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- BigCatalogAPI/Program.cs
+++ BigCatalogAPI/Program.cs
@@ -1,3 +1,6 @@
+using BigCatalogAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer
+    ("Server=DESKTOP-332I3M0\\SQLEXPRESS;Database=Northwind;Encrypt=False; Trusted_Connection=True;"));
 
 var app = builder.Build();
 
